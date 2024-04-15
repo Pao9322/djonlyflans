9b56a9835d68a8a34d8d2a57a21c8bb5a0823e69
Paola Acevedo P <ppaaoolaa3@gmail.com>
2024-04-14 23:03:17 -0400

    requerimiento 2 hito 2

--- djonlyflans/djonlyflans/urls.py
+++ djonlyflans/djonlyflans/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pagina.views import indice, acerca, bienvenido
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   
 ]
